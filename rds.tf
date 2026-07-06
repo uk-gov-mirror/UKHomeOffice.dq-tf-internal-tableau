b406eb1d7274be0dcf6a4df08b601d24c387328f
Peter Miller <98960886+PeterMillerHO@users.noreply.github.com>
2026-07-06 11:40:44 +0100

    Upgrade storage for internal_tbaleau RDS in Prod (#663)
    * Upgrade storage for internal_tbaleau RDS in Prod
    
    * Update test for increased storage
    
    ---------
    
    Co-authored-by: D-Peter Miller <d-millerp2@U-3DPNVKJVHVYBU.dev.dacc.phz>

--- rds.tf
+++ rds.tf
@@ -104,7 +104,7 @@ resource "aws_iam_role_policy_attachment" "dq_tf_infra_write_to_cw_rds" {
 resource "aws_db_instance" "postgres" {
   identifier                      = "postgres-${local.naming_suffix}"
   auto_minor_version_upgrade      = "false"
-  allocated_storage               = var.environment == "prod" ? "4000" : "5000"
+  allocated_storage               = var.environment == "prod" ? "5000" : "5000"
   storage_type                    = var.environment == "prod" ? "gp2" : "gp3"
   engine                          = "postgres"
   engine_version                  = var.environment == "prod" ? "14.22" : "14.22"
